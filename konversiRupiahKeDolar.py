18d2d6f8bd6cc79277197d48b535708e1b293f72
egapermana <52640604+egapermana@users.noreply.github.com>
2019-07-08 08:05:55 +0700

    Create konversiRupiahKeDolar.py

---
+++ konversiRupiahKeDolar.py
@@ -0,0 +1,32 @@
+# Jawaban Soal nonor 01
+# Nama file: Ega Permana soal 01
+# Nama: Ega Permana
+# Nomor Absen: 4
+# Kelas: CTA 3
+# Instruktur : Nurhamid, P.Hd
+
+# baris di bawah ini Anda mulai membuat memrogram:
+print("======================================")
+print("PROGRAM KONVERSI RUPIAH KE DOLAR")
+print("======================================")
+print("Untuk di ketahui harga 1 Dolar saat ini adalah Rp. 14.591")
+rupiahUser=int(input('Masukan nominal uang dalam, Rp : '))
+dolarUser=round((rupiahUser/14591),2)
+print("Hasil konversi", 'Rp', rupiahUser, 'adalah',dolarUser,"dolar" )
+print("Terimakasih")
+# dan seterusnya
+# dan seterusnya
+
+
+# Output hasil keluaran
+
+"""
+======================================
+PROGRAM KONVERSI RUPIAH KE DOLAR
+======================================
+Untuk di ketahui harga 1 Dolar saat ini adalah Rp. 14.591
+Masukan nominal uang dalam, Rp : 20000
+Hasil konversi Rp 20000 adalah 1.37 dolar
+Terimakasih
+
+"""
